d2cee0245b7a7cbb810c8cc5e216d24d35871056
Cenk Gokturk <cenkgokturk06@gmail.com>
2021-02-26 00:19:24 +0300

    Admin page has been updated with additional information

--- Controllers/AdminPageController.cs
+++ Controllers/AdminPageController.cs
@@ -6,6 +6,10 @@ using Microsoft.Data.SqlClient;
 using ASPNETAOP.Models;
 using System.Runtime.InteropServices;
 using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace ASPNETAOP.Controllers
 {
@@ -22,6 +26,44 @@ namespace ASPNETAOP.Controllers
             return View();
         }
 
+        private void retrieveSessionInfo(int userID)
+        {
+            foreach (Pair pair in SessionList.listObject.Pair)
+            {
+                if (pair.getUserID().Equals(userID))
+                {
+                    HttpClient client = new HttpClient();
+                    String connectionString = "https://localhost:44316/api/SessionItems/" + SessionList.listObject.count;
+                    Task<SessionItem> userSession = GetJsonHttpClient(connectionString, client); ;
+
+                    //ViewData["message"] = "User name: " + userSession.Result.Username + "\r\n Mail: " + userSession.Result.Usermail;
+                }
+            }
+        }
+
+        //Used to extract user information from retrieved json file
+        private static async Task<SessionItem> GetJsonHttpClient(string uri, HttpClient httpClient)
+        {
+            try
+            {
+                return await httpClient.GetFromJsonAsync<SessionItem>(uri);
+            }
+            catch (HttpRequestException) // Non success
+            {
+                Console.WriteLine("An error occurred.");
+            }
+            catch (NotSupportedException) // When content type is not valid
+            {
+                Console.WriteLine("The content type is not supported.");
+            }
+            catch (JsonException) // Invalid JSON
+            {
+                Console.WriteLine("Invalid JSON.");
+            }
+
+            return null;
+        }
+
         [IsAuthenticated]
         [IsAuthorized]
         public IActionResult UserList()
@@ -33,7 +75,7 @@ namespace ASPNETAOP.Controllers
             var model = new List<AdminPage>();
             using (SqlConnection sqlconn = new SqlConnection(connection))
             {
-                string sqlquery = "select AI.Username, AI.Usermail, AcS.LoginDate, AcS.IsLoggedIn from AccountInfo AI, AccountSessions AcS WHERE AI.Usermail=AcS.Usermail";
+                string sqlquery = "select AI.UserID, AI.Username, AI.Usermail, AR.Rolename, AcS.LoginDate, AcS.IsLoggedIn from AccountInfo AI, AccountSessions AcS, UserRoles UR, AccountRoles AR where AI.Usermail=AcS.Usermail AND UR.UserID=AI.UserID AND AR.Roleid=UR.Roleid;";
                 using (SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn))
                 {
                     sqlconn.Open();
@@ -44,8 +86,10 @@ namespace ASPNETAOP.Controllers
                         while (reader.Read())
                         {
                             var user = new AdminPage();
+                            user.UserID = (int)reader["UserID"];
                             user.Username = (string)reader["Username"];
                             user.Usermail = (string)reader["Usermail"];
+                            user.Rolename = (string)reader["Rolename"];
                             user.LoginDate = (string)reader["LoginDate"];
                             user.IsLoggedIn = (int)reader["IsLoggedIn"];
 
--- Models/AdminPage.cs
+++ Models/AdminPage.cs
@@ -7,8 +7,10 @@ namespace ASPNETAOP.Models
 {
     public class AdminPage
     {
+        public int UserID { get; set; }
         public string Username { get; set; }
         public string Usermail { get; set; }
+        public string Rolename { get; set; }
         public string LoginDate { get; set; }
         public int IsLoggedIn { get; set; }
 
