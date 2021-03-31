4509e5990ea9e0287585c8b9c75482e76724763c
elifnurafsar <eafsar@etu.edu.tr>
2021-03-31 22:22:00 +0300

    Added NHibernate files. A problem occurred when using Http.Current.Server.

--- Models/AntennaScan.cs
+++ Models/AntennaScan.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
+
 
 namespace ASPNETAOP.Models
 {
--- Models/NHibernateSession.cs
+++ Models/NHibernateSession.cs
@@ -1,31 +1,66 @@
 ﻿using NHibernate;
 using NHibernate.Cfg;
+using ASPNETAOP.Session;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNetCore.Http;
 
-namespace AspNetCore.Models
+namespace ASPNETAOAP.Models
 {
     public class NHIbernateSession
     {
-        /*public static ISession OpenSession()
+        
+        public static ISession OpenSession()
         {
 
             var configuration = new Configuration();
 
-            var configurationPath = HttpContext.Current.Server.MapPath(@"~\Models\Nhibernate\hibernate.cfg.xml");
+            var configurationPath = AppHttpContext.Current.Server.MapPath(@"~\Models\Nhibernate\hibernate.cfg.xml");
 
             configuration.Configure(configurationPath);
 
-            var employeeConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\Employee.hbm.xml");
+            var antennaConfigurationFile = AppHttpContext.Current.Server.MapPath(@"~\Models\NHibernate\AddAntenna.hbm.xml");
 
-            configuration.AddFile(employeeConfigurationFile);
+            configuration.AddFile(antennaConfigurationFile);
+
+            var transmitterConfigurationFile = AppHttpContext.Current.Server.MapPath(@"~\Models\NHibernate\AddTransmitter.hbm.xml");
+
+            configuration.AddFile(transmitterConfigurationFile);
+
+            var receiverConfigurationFile = AppHttpContext.Current.Server.MapPath(@"~\Models\NHibernate\AddReceiver.hbm.xml");
+
+            configuration.AddFile(receiverConfigurationFile);
+
+            var antenna_scanConfigurationFile = AppHttpContext.Current.Server.MapPath(@"~\Models\NHibernate\AddAntennaScan.hbm.xml");
+
+            configuration.AddFile(antenna_scanConfigurationFile);
+
+            var locationConfigurationFile = AppHttpContext.Current.Server.MapPath(@"~\Models\NHibernate\AddLocation.hbm.xml");
+
+            configuration.AddFile(locationConfigurationFile);
+
+            var modeConfigurationFile = AppHttpContext.Current.Server.MapPath(@"~\Models\NHibernate\AddMode.hbm.xml");
+
+            configuration.AddFile(modeConfigurationFile);
+
+            var submodeConfigurationFile = AppHttpContext.Current.Server.MapPath(@"~\Models\NHibernate\AddSubmode.hbm.xml");
+
+            configuration.AddFile(submodeConfigurationFile);
+
+            var scanConfigurationFile = AppHttpContext.Current.Server.MapPath(@"~\Models\NHibernate\AddScan.hbm.xml");
+
+            configuration.AddFile(scanConfigurationFile);
+
+            var radarConfigurationFile = AppHttpContext.Current.Server.MapPath(@"~\Models\NHibernate\AddRadar.hbm.xml");
+
+            configuration.AddFile(radarConfigurationFile);
 
             ISessionFactory sessionFactory = configuration.BuildSessionFactory();
 
             return sessionFactory.OpenSession();
 
-        }*/
+        }
     }
 }
