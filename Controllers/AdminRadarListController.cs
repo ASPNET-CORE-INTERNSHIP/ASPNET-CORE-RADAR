c2c4aac4c8ace847cb738ed821dc92294ab6a558
Cenk Gokturk <cenkgokturk06@gmail.com>
2021-04-13 22:16:45 +0300

    Preliminary preparation for displaying radars

---
+++ Controllers/AdminRadarListController.cs
@@ -0,0 +1,39 @@
+﻿using ASPNETAOP.Aspect;
+using ASPNETAOP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System;
+using ASPNETAOP.Session;
+
+namespace ASPNETAOP.Controllers
+{
+    [Guid("151E61F1-1FE3-4FAD-B8EC-9034B676579E")]
+    public class AdminRadarListController : Controller
+    {
+        private readonly Session.NHibernateMapperSession _session;
+
+        public AdminRadarListController(NHibernateMapperSession session)
+        {
+            _session = session;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }   
+
+        [IsAuthenticated]
+        [IsAuthorized("151E61F1-1FE3-4FAD-B8EC-9034B676579E")]
+        public IActionResult RadarListAsync()
+        {
+            TempData["ResultMessage"] = "Regular";
+
+            var model = new List<AdminRadarList>();
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/AdminRadarList.cs
@@ -0,0 +1,13 @@
+﻿namespace ASPNETAOP.Models
+{
+    public class AdminRadarList
+    {
+        public Radar radar { get; set; }
+        public Receiver receiver { get; set; }
+        public Transmitter transmitter { get; set; }
+        public Location location { get; set; }
+
+        public Submode submode { get; set; }
+
+    }
+}
